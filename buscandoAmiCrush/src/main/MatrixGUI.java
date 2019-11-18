0961e07c6028ec1929586b763376896ef5e40d5a
Luis Andrés Fallas Valenciano <andresfallasv@gmail.com>
2019-11-17 21:56:19 -0600

    -Se llena matriz con recursividad

--- buscandoAmiCrush/src/main/MatrixGUI.java
+++ buscandoAmiCrush/src/main/MatrixGUI.java
@@ -10,6 +10,8 @@ package main;
 //import javax.swing.JToggleButton;
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 
@@ -19,15 +21,16 @@ import javax.swing.border.LineBorder;
  */
 public class MatrixGUI extends JFrame {
 
+    private JFrame jFrame = this;
     private JPanel jPanel;
     private JButton[][] jButton;
 
     public MatrixGUI() {
-        //buildMatrix();
         setjPanel(getJButton());
-        add(jPanel);
+        setKeyPressEvent();
+        this.add(jPanel);
         setTommy();
-        iniciarJFrame();
+        getJFrame();
 
     }
 
@@ -50,7 +53,7 @@ public class MatrixGUI extends JFrame {
         return jPanel;
     }
 
-    public void iniciarJFrame() {
+    public void getJFrame() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(1600, 1000);
         this.setLayout(null);
@@ -62,15 +65,37 @@ public class MatrixGUI extends JFrame {
     public JButton[][] getJButton() {
         if (jButton == null) {
             jButton = new JButton[20][20];
-            for (int i = 0; i < jButton.length; i++) {
-                for (int j = 0; j < jButton.length; j++) {
-                    jButton[i][j] = new JButton();
-                    jButton[i][j].setName(i + "," + j);
-                    jButton[i][j].setContentAreaFilled(false);
-                    jButton[i][j].setBackground(Color.GRAY);
-                    jButton[i][j].setOpaque(true);
-                }
+            llenaMatriz(jButton, 0, 0);
+        }
+        return jButton;
+    }
+
+    /**
+     * Usa recursividad para llenar la matriz.
+     *
+     * @param matriz
+     * @param i
+     * @param j
+     */
+    public JButton[][] llenaMatriz(JButton[][] matriz, int i, int j) {
+        jButton[i][j] = new JButton();
+        jButton[i][j].setName(i + "," + j);
+        jButton[i][j].setContentAreaFilled(false);
+        jButton[i][j].setBackground(Color.GRAY);
+        jButton[i][j].setOpaque(true);
+
+        //Mientras las filas no lleguen a su tope -1 y las columnas no sean igual al tope de filas -1 de la matriz
+        if (i != matriz.length - 1 || j != matriz[i].length - 1) {
+            //Si el número de columna es igual al tope de filas -1 de la matriz
+            if (j == matriz[i].length - 1) {
+                i++;
+                j = 0;
+
+            } else {
+                j++;
             }
+
+            llenaMatriz(matriz, i, j);
         }
         return jButton;
     }
@@ -79,11 +104,39 @@ public class MatrixGUI extends JFrame {
         jButton[10][10].setText("Tommy");
         jButton[10][10].setOpaque(true);
         jButton[10][10].setBackground(Color.red);
+    }
 
+    public JButton getTommy() {
+        return jButton[10][10];
     }
 
     public void moveTommy() {
+        //Permite detectar el presionado de tecla
 
+    }
+
+    public void setKeyPressEvent() {
+        jPanel.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent ke) {
+                if (ke.getKeyCode() == KeyEvent.VK_ENTER) {
+                    getTommy().setBackground(Color.blue);
+                    jFrame.revalidate();
+                    jFrame.repaint();
+                    System.out.println("ENTER!!!");
+                }
+            }
+
+            @Override
+            public void keyPressed(KeyEvent ke) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void keyReleased(KeyEvent ke) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        });
     }
 
 }
